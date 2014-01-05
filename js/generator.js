7e8b534868aaa6476edafdfa68819f2026a363cb
Nick Stevens <nick@nickstevens.com>
2014-01-05 13:19:19 -0700

    Renamed home to index to move to server. Updated JSON to use master property.

--- js/generator.js
+++ js/generator.js
@@ -1,3 +1,9 @@
 var Generator = function() {
 	this.latin = false;
+
+	// listen for click of 'generate'
+
+	// get user input for number of paragraphs and latin
+
+	// insert paragraphs into page
 };
